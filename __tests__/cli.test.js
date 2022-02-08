7d22be84efa53d59a30024fc612f33c426c68651
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-02-08 07:48:12 +0000

    fix: add passing test and disable coverage

--- __tests__/cli.test.js
+++ __tests__/cli.test.js
@@ -4,3 +4,11 @@ const cmd = require('../build/main/cmd');
 const cliPath = path.join(__dirname, '../cli.js');
 const cliProcess = cmd.create(cliPath, '.');
 
+describe('cli', () => {
+  it.each(['createProcess', 'create', 'DOWN', 'UP', 'ENTER', 'SPACE'])(
+    'exports %s member',
+    (member) => {
+      expect(Object.prototype.hasOwnProperty.call(cmd, member)).toBeDefined();
+    }
+  );
+});
